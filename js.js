82799c636e4660d0a6631d3d3c5eead5e36876d1
html-creator <122980519+html-creator@users.noreply.github.com>
2023-01-29 19:37:39 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,22 @@
+function test(min) {
+
+    if (age > (min - 1)) {
+
+        console.clear()
+
+        alert("siema")
+
+    } else {
+
+        console.clear()
+
+        alert("nie lubiê maluchów")
+
+    }
+
+}
+function getAge() {
+	age = document.getElementById("age").value;
+	mikro = Math.floor((Math.random() * 20) + 5);
+	test(mikro)
+}
